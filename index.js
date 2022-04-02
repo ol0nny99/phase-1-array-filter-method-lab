0d8bb35fd70535cdcbd0725d1342d19e025063d1
ol0nny99 <100658149+ol0nny99@users.noreply.github.com>
2022-04-02 12:38:20 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+function findMatching(collection, stringToMatch) {
+const matchList = collection.filter (function (user){
+    return user.toLowerCase() === stringToMatch.toLowerCase();
+  });
+
+  return matchList;
+}
+
+function fuzzyMatch(array, string) {
+    const matchReturns = array.filter( function (user) {
+        return user.startsWith(string);
+    });
+
+    return matchReturns;
+}
+
+function matchName(array, string) {
+
+    const matchedReturn = array.filter( function (user) {
+        return user.name === string;
+    })
+  
+    return matchedReturn;
+  }
